3990173b6ea1e0a5672ad837285388b7fdcb3633
YWU99u <yuqi14@ualberta.ca>
2024-04-27 15:39:43 -0600

    updates

--- src/SC_generator.py
+++ src/SC_generator.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     df_subset = df[:NUM_OF_SAMPLES]
 
     # Data collection
-    for row_idx in tqdm(range(18,73), colour='blue', desc='Sample Progress', position=0):
+    for row_idx in tqdm(range(25,73), colour='blue', desc='Sample Progress', position=0):
         row = df_subset.iloc[row_idx]
         subject = row['Category']
         question = row['Question']
